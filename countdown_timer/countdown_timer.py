6ebe4a8f8edf8bd5fb68690d468acceab2ff25b1
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-25 15:21:15 +0530

    Countdown time now printed in the format "HH MM SS"
    Should work up to 1 hours.
    
    Need to work on countdown time > 59 m 59 s.

--- countdown_timer/countdown_timer.py
+++ countdown_timer/countdown_timer.py
@@ -1,73 +1,48 @@
 # Simple program that works like a countdown timer.
-import time # imports time module
+import time
+# imports time module
 
-hr = int(input("Please enter hours (hh): "))                              # Requests hours from user.
-min = int(input("Please enter minuues (mm): "))                            # Requests minutes from user.
-sec = int(input("Please enter seconds (ss): "))                            # Requests seconds from user.
+hr = int(input("Please enter hours (hh): "))
+# Requests hours from user.
+min = int(input("Please enter minuues (mm): "))
+# Requests minutes from user.
+sec = int(input("Please enter seconds (ss): "))
+# Requests seconds from user.
 
-# ct = time.ctime() # Assigns current time as a string "DAY MON DD HH:MM:SS YYYY"
-
-t1 = time.time()                                                              # Current time in seconds.
-
-# h1 = ct[11: 13]                                                             # Slices the hours from ct.
-# m1 = ct[14: 16]                                                             # Slices the minutes from ct.
-# s1 = ct[17:19]                                                              # Slices the seconds from ct
+t1 = round(time.time())
+# Current time in seconds.
 
 
-ct_time = (hr * 3600) + (min * 60) + sec                                             # Countdown time in seconds
-t2 = t1 + ct_time                                                                    # time in seconds since epoch when countdown ends.
+ct_time = (hr * 3600) + (min * 60) + sec
+# Countdown time in seconds
+t2 = t1 + ct_time
+# time in seconds since epoch when countdown ends.
 
 while True:
 
-    t1 = time.time()
     n = 1
 
-    if (t2 - t1) > 0:        
+    t1 = round(time.time())
+    h_rem = 0
+    m_rem = 0
+    s_rem = t2-t1
 
-        print(f"Time remaining: {round(t2-t1)} seconds")
+    if (t2 - t1) > 0:
+
+        time_rem = t2-t1
+
+        if time_rem > 59:
+            m_rem = time_rem / 60
+            s_rem = time_rem - (int(m_rem) * 60)
+            if m_rem > 59:
+                h_rem = 1+ ((m_rem - 60) / 60)
+
+        print(f"Time remaining: {(h_rem)}:{int(m_rem)}:{(s_rem)}")
         time.sleep(1)
 
     else:
-        print(f"time remaining: {round(t2-t1)} seconds")
+        print(f"Time remaining: {(h_rem)}:{int(m_rem)}:{(s_rem)}")
         print("Countdown Ends!")
         break
     
-    n += 1
-
-
-
-# h2 = hr + int(h1)                                                                # hour at which countdown should stop.
-# m2 = min + int(m1)                                                               # minute of h2 at which countdown should stop.
-# s2 = sec + int(s1)                                                               # second of h2 and m2 at which countdown should stop.
-
-# delta_h = h2 - int(h1)                                                           # time in hours left for countdown.
-# delta_m = m2 - int(m1)                                                           # time in minutes left for countdown.
-# delta_s = s2 - int(s1)                                                           # time in seconds left for countdown.
-
-
-# while True:
-    
-#     n = 1
-
-#     if (t2 -t1)> 1:                                 # Checks if time left is more that one sec.
-
-#         ct = time.ctime()                                                           # Assigns current time as a string "DAY MON DD HH:MM:SS YYYY"
-#         h1 = ct[11: 13]                                                             # Slices the hours from ct.
-#         m1 = ct[14: 16]                                                             # Slices the minutes from ct.
-#         s1 = ct[17:19]                                                              # Slices the seconds from ct
-
-#         delta_h = h2 - int(h1)                                                           # time in hours left for countdown.
-#         delta_m = m2 - int(m1)                                                           # time in minutes left for countdown.
-#         delta_s = s2 - int(s1)                                                           # time in seconds left for countdown.
-
-#         print(f"Time Remaining: {delta_h}:{delta_m}:{delta_s}")
-
-#         time.sleep(1)
-
-#     else:
-#         delta_s -= 1
-#         print(f"Time Remaining: {delta_h}:{delta_m}:{delta_s}")
-#         print("Countdown Ended!")
-#         break
-
     n += 1
